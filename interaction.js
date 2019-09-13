84b469e582b590b12b8be4e74ab64acc04eab347
Daniela GV <danielagava96@gmail.com>
2019-09-13 07:48:13 -0500

    punto en el js

--- interaction.js
+++ interaction.js
@@ -7,25 +7,25 @@ var principal = document.querySelector('.interImg');
     function modCero(){
         //console.log(this.getAttribute("src"));
         console.log('principal');
-        principal.setAttribute("src", '/img/interaction0.jpg');
+        principal.setAttribute("src", './img/interaction0.jpg');
       
     }
     function modFirst(){
         //console.log(this.getAttribute("src"));
         console.log('principal');
-        principal.setAttribute("src", '/img/interaction1.jpg');
+        principal.setAttribute("src", './img/interaction1.jpg');
       
     }
     function modSecond(){
         //console.log(this.getAttribute("src"));
         console.log('principal');
-        principal.setAttribute("src", '/img/interaction2.jpg');
+        principal.setAttribute("src", './img/interaction2.jpg');
       
     }
     function modThird(){
         //console.log(this.getAttribute("src"));
         console.log('principal');
-        principal.setAttribute("src", '/img/interaction3.jpg');
+        principal.setAttribute("src", './img/interaction3.jpg');
       
     }
     cero.addEventListener('click',modCero);
